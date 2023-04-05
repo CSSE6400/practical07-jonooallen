a4a296c70c14295465d6423d1667f973a67801fd
jonooallen <jonooallen@gmail.com>
2023-04-05 11:51:34 +1000

    Week 7

---
+++ main.tf
@@ -0,0 +1,31 @@
+terraform { 
+  required_providers { 
+    aws = { 
+      source = "hashicorp/aws" 
+      version = "~> 4.0" 
+    } 
+    docker = { 
+      source = "kreuzwerker/docker" 
+      version = "3.0.2" 
+    } 
+  } 
+} 
+ 
+provider "aws" { 
+  region = "us-east-1" 
+  shared_credentials_files = ["./credentials"] 
+}
+
+data "aws_ecr_authorization_token" "ecr_token" {} 
+ 
+provider "docker" { 
+ registry_auth { 
+   address = data.aws_ecr_authorization_token.ecr_token.proxy_endpoint 
+   username = data.aws_ecr_authorization_token.ecr_token.user_name 
+   password = data.aws_ecr_authorization_token.ecr_token.password 
+ } 
+}
+
+resource "aws_ecr_repository" "taskoverflow" { 
+ name = "taskoverflow" 
+}
